b0b15796da4fd0593a0d72266d7735f351597f7c
khaouitiabdelhakim <abdelhakimkhaouiti62@gmail.com>
2025-01-04 19:44:18 +0100

    fixed window

--- app/user/tracking/page.tsx
+++ app/user/tracking/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import BusTracking from "@/components/BusTracking";
-import React from "react";
+import dynamic from 'next/dynamic';
+import React from 'react';
+
+const DynamicBusTracking = dynamic(() => import('@/components/BusTracking'), { ssr: false });
 
 const App = () => {
   const busId = 'BC1234'; // Replace with the actual bus ID you want to track
   return (
     <div>
       <h1>Bus Tracking</h1>
-      <BusTracking busId={busId} />
+      <DynamicBusTracking busId={busId} />
     </div>
   );
 };
